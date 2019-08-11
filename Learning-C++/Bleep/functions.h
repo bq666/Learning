b71f74ec4f40c2041dad97bd0d9329003465dd8e
bq666 <wojciech.gaudnik@gmail.com>
2019-08-11 22:42:11 +0200

    C++ Bleep

---
+++ Learning-C++/Bleep/functions.h
@@ -0,0 +1,13 @@
+//
+// Created by bq666 on 8/11/19.
+//
+
+#ifndef C_C___FUNCTIONS_H
+#define C_C___FUNCTIONS_H
+
+#include <string>
+
+void bleep(std::string word, std::string &text);
+void asterisk(std::string word, std::string &text, int i);
+
+#endif //C_C___FUNCTIONS_H
---
+++ Learning-C++/cmake-build-debug/CMakeFiles/MyFirstTarget.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyFirstTarget.dir/whale.cpp.o"
+  "CMakeFiles/MyFirstTarget.dir/Bleep/bleep.cpp.o"
+  "CMakeFiles/MyFirstTarget.dir/Bleep/functions.cpp.o"
+  "MyFirstTarget.pdb"
+  "MyFirstTarget"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyFirstTarget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
