aaffa9913104942303b361827cf695804c86e4d1
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-12-18 09:02:52 +0100

    next

---
+++ Learning-C++/fraction/cmake-build-debug/CMakeFiles/fraction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fraction.dir/main.c.o"
+  "fraction"
+  "fraction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
